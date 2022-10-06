7194827807f683e9dc739227ce84912610f8d45c
Glenn <glennp167@gmail.com>
2022-10-06 13:10:25 -0300

    Updates

--- src/interfaces/Search.tsx
+++
@@ -1,9 +0,0 @@
-import { KeyboardEvent, MouseEventHandler } from "react";
-
-export interface SearchInterface {
-  search?: string;
-  setSearch?: (arg0: any) => void,
-  objects?: string | any[],
-  myKey?: (arg0: KeyboardEvent<HTMLInputElement>) => void,
-  reset?: MouseEventHandler<HTMLButtonElement>,
-}
